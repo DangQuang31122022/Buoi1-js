f37c8cbd1e8594b365a94e5394fc54386561a3cf
dangquang <dangquang@duck.com>
2024-08-21 08:06:48 +0700

    fundamentals-part2

---
+++ fundamentals-part2/script.js
@@ -0,0 +1,90 @@
+// CHALLENGE 1
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+
+// data 1
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+
+// data 2
+// let scoreDolphins = calcAverage(85, 54, 41);
+// let scoreKoalas = calcAverage(23, 34, 27);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+  } else {
+    console.log("No team wins...");
+  }
+};
+checkWinner(scoreDolphins, scoreKoalas);
+
+// CHALLENGE 2
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, totals);
+
+// CHALLENGE 3
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+mark.calcBMI();
+john.calcBMI();
+
+if (mark.bmi > john.bmi) {
+  console.log(
+    `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`
+  );
+} else if (john.bmi > mark.bmi) {
+  console.log(
+    `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`
+  );
+}
+
+// CHALLENGE 4
+const bills4 = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips4 = [];
+const totals4 = [];
+
+for (let i = 0; i < bills4.length; i++) {
+  const tip = calcTip(bills4[i]);
+  tips4.push(tip);
+  totals4.push(tip + bills4[i]);
+}
+console.log(bills4, tips4, totals4);
+
+const calcAverage4 = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // sum = sum + arr[i];
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+console.log(calcAverage4([2, 3, 7]));
+console.log(calcAverage4(tips4));
+console.log(calcAverage4(totals4));
